4be20964c719422c75b26c359cd4abb43654e2f8
AlandvMarques <137972339+AlandvMarques@users.noreply.github.com>
2023-11-09 14:47:34 -0300

    explorando o console

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,27 @@
+//mais de um valor
+
+const x = 10
+const y = 'layne'
+const z = [1,2]
+const u = 'oiiiii'
+
+console.log(x,y,z)
+
+
+//contagem de impressões 
+
+console.count(`Valor de x é: ${x}, contagem`)
+console.count(`Valor de x é: ${x}, contagem`)
+console.count(`Valor de x é: ${x}, contagem`)
+console.count(`Valor de x é: ${x}, contagem`)
+console.count(`Valor de x é: ${x}, contagem`)
+
+
+//variavel entre string
+
+console.log('O nome é %i, ele é programador', y)
+
+//limpar console 
+setTimeout(()=>{
+    console.clear()
+},2000)
